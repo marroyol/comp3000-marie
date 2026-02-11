34b67fa46d17eed32631f179f61706fae7455177
marroyol <frances.arroyolopez@students.plymouth.ac.uk>
2026-02-11 13:23:26 +0000

    added functions for landmark labelling

--- code/facial_landmark_labeller.py
+++ code/facial_landmark_labeller.py
@@ -14,3 +14,12 @@ LANDMARK_INDEX_MAP = Dict[int, str] = {
 LANDMARK_NAME_MAP = Dict[int, str] = {}
 for index, name in LANDMARK_INDEX_MAP.items():
     LANDMARK_NAME_MAP[name] = index
+
+def get_landmark_name(index:int) -> str:
+    return LANDMARK_INDEX_MAP[index]
+
+def get_landmark_index(name:str) -> int:
+    return LANDMARK_NAME_MAP[name]
+
+def get_defined_indices() -> List[int]:
+    return list(LANDMARK_INDEX_MAP.key())
