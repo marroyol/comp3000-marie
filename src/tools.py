86f0c3063cddddadb011b61c42777a2b8ef9c513
marroyol <frances.arroyolopez@students.plymouth.ac.uk>
2026-03-25 20:01:02 +0000

    fixed some bugs with the dot product, added midpoint

--- src/tools.py
+++ src/tools.py
@@ -14,19 +14,32 @@ def find_matching_image(label_filename, image_dir):
             return image_path
     return None
 
-def compute_angle(ax, ay, bx, by):
+def compute_angle(vertex, point_a, point_b):
     """Attempting to get angles using dot product"""
 
-    vector_a = [ax, ay]
-    vector_b = [bx, by]
+    vx, vy = vertex
+    ax, ay = point_a
+    bx, by = point_b
+
+    vector_a = [ax - vx, ay - vy]
+    vector_b = [bx - vx, by - vy]
 
     dot_product = (vector_a[0] * vector_b[0]) + (vector_a[1]*vector_b[1])
 
     norm_a = math.sqrt(vector_a[0]**2 + vector_a[1]**2)
     norm_b = math.sqrt(vector_b[0]**2 + vector_b[1]**2)
 
+    if norm_a == 0 or norm_b == 0:
+        return float("nan")
+
     cos_theta = dot_product / (norm_a * norm_b)
+    cos_theta = max(-1.0, min(1.0, cos_theta)) # to prevent cos_theta crashing
     # it's in degrees to match the literature (Evangelista)
     theta = math.degrees(math.acos(cos_theta))
 
     return theta
+
+def midpoint(point_a, point_b):
+    ax, ay = point_a
+    bx, by = point_b
+    return ((ax+bx)/2.0, (ay+by/2.0))
